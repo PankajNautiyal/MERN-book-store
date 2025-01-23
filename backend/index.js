35273ae4d502b9be5cabf175b99c3fa79dd1b9bb
PankajNautiyal <pankajnautiyal62@gmail.com>
2025-01-23 22:22:35 +0530

    updating dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,41 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
-import {PORT, mongoDBURL} from './config.js';
 import mongoose from 'mongoose';
 import { Book } from './models/bookModel.js';
 import bookRoute from './routes/booksRoute.js'
-import cors from 'cors' 
+import cors from 'cors';
+
 const app = express();
 
 // Middleware for parsing req body
 app.use(express.json());
 
-// app.use(cors);
-
+// CORS settings
 app.use(cors({
     origin: 'http://localhost:5173',
-    methods: ['GET','POST','PUT','DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
-}))
-app.get('/', (req,res) => {
-    console.log(req)
-    return res.status(234).send('Welcome to MERN Stack')
+}));
+
+app.get('/', (req, res) => {
+    console.log(req);
+    return res.status(234).send('Welcome to MERN Stack');
 });
 
-app.use('/books', bookRoute)
+app.use('/books', bookRoute);
 
+const PORT = process.env.PORT || 5000;
+const mongoDBURL = process.env.MONGO_DB_URL;
 
+// MongoDB connection
 mongoose.connect(mongoDBURL)
-.then(()=> {
-    console.log('App connected to database')
-    app.listen(PORT,()=>{
-        console.log(`App is listening to port: ${PORT}`)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+            console.log(`App is listening on port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
     });
-    
-})
-.catch((error)=> {
-    console.log(error)
-})
