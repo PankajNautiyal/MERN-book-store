6cd672af1131430d180392e4a0f8f033dda9afa5
PankajNautiyal <pankajnautiyal62@gmail.com>
2025-02-23 23:38:06 +0530

    updated mongoDb option

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(cors({
 }));
 
 app.get('/', (req, res) => {
-    console.log(req);
+    // console.log(req);
     return res.status(234).send('Welcome to MERN Stack');
 });
 
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 const mongoDBURL = process.env.MONGO_DB_URL;
 
 // MongoDB connection
-mongoose.connect(mongoDBURL)
+mongoose.connect(mongoDBURL,{ useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('App connected to database');
         app.listen(PORT, () => {
